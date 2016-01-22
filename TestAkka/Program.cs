ac07897293b34b9a45ea61145f4816dc89cc730a
李兴钢 <lishinko@qq.com>
2016-01-22 23:33:36 +0800

    akka.net from getting started

---
+++ TestAkka/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Akka;
+using Akka.Actor;
+
+namespace TestAkka
+{
+  public class Greet
+  {
+    public Greet(string who)
+    {
+      Who = who;
+    }
+    public string Who {get; private set;}
+  }
+	public class GreetingActor : ReceiveActor
+	{
+		public GreetingActor()
+		{
+			Receive<Greet> (greet =>
+				Console.WriteLine (" hello {0}", greet.Who)
+			);
+		}
+	}
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+				var system = ActorSystem.Create ("MySystem");
+				var greeter = system.ActorOf<GreetingActor> ("greeter");
+				greeter.Tell (new Greet ("李兴钢"));
+				Console.ReadLine ();
+		}
+	}
+}
